c929ac912763321f94a7fef14091873e4f946115
shijing0628 <brendaliu0628@gmail.com>
2021-02-04 11:35:56 -0600

    solution

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql')
 //lodash loop through an array easier
 const _ = require('lodash')
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt,
- GraphQLList } = graphql
+ GraphQLList, GraphQLNonNull } = graphql
 const Book = require('../../server/models/book')
 const Author = require('../../server/models/author')
 
@@ -99,8 +99,8 @@ const Mutation = new GraphQLObjectType({
   addAuthor: {
    type: AuthorType,
    args: {
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    age: { type: new GraphQLNonNull(GraphQLInt) }
    },
    resolve(parent, args) {
     let author = new Author({
@@ -114,9 +114,9 @@ const Mutation = new GraphQLObjectType({
   addBook: {
    type: BookType,
    args: {
-    name: { type: GraphQLString },
-    genre: { type: GraphQLString },
-    authorId: { type: GraphQLID }
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    genre: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLID) }
    },
    resolve(parent, args) {
     let book = new Book({
